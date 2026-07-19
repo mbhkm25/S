1cbccf7de739c978e3ed3e249ed583ff595bd65a
mbhkm25 <m.bhkm25@gmail.com>
2026-07-20 02:28:20 +0300

    Align business community with standard mobile gutter

--- src/components/business/BusinessCommunity.tsx
+++ src/components/business/BusinessCommunity.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function BusinessCommunity({ onNavigate }: Props) {
   return (
-    <div className="-mx-3 sm:mx-0 [&_.max-w-5xl]:!max-w-none [&>div>header]:!px-1 [&>div>main]:!px-1 sm:[&>div>header]:!px-3 sm:[&>div>main]:!px-3 lg:[&>div>header]:!px-5 lg:[&>div>main]:!px-5">
+    <div className="mx-0 [&_.max-w-5xl]:!max-w-none [&>div>header]:!px-2 [&>div>main]:!px-2 sm:[&>div>header]:!px-3 sm:[&>div>main]:!px-3 lg:[&>div>header]:!px-5 lg:[&>div>main]:!px-5">
       <BusinessCommunityV2 onNavigate={onNavigate} />
     </div>
   );
