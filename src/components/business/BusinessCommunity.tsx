3e91289065a6a6f2d4a5e3b37cc95a144162616f
mbhkm25 <m.bhkm25@gmail.com>
2026-07-19 22:13:06 +0300

    Expand business community content on mobile

--- src/components/business/BusinessCommunity.tsx
+++ src/components/business/BusinessCommunity.tsx
@@ -1,1 +1,13 @@
-export { default } from './BusinessCommunityV2';
+import BusinessCommunityV2 from './BusinessCommunityV2';
+
+interface Props {
+  onNavigate: (page: string, token?: string) => void;
+}
+
+export default function BusinessCommunity({ onNavigate }: Props) {
+  return (
+    <div className="-mx-3 sm:mx-0 [&_.max-w-5xl]:!max-w-none [&>div>header]:!px-1 [&>div>main]:!px-1 sm:[&>div>header]:!px-3 sm:[&>div>main]:!px-3 lg:[&>div>header]:!px-5 lg:[&>div>main]:!px-5">
+      <BusinessCommunityV2 onNavigate={onNavigate} />
+    </div>
+  );
+}
