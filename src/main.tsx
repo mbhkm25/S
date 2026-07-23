4b82f55f5335a8af8cd5f4471dcc4c9dd1b29727
mbhkm25 <m.bhkm25@gmail.com>
2026-07-23 19:31:49 +0300

    Enable resilient public profile sharing

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './lib/publicBusinessProfileSwipe';
+import './lib/publicBusinessProfileShare';
 
 import { Capacitor } from '@capacitor/core';
 
