47d8d16d5d754773a93b58112b9f0e66254fb0d3
mbhkm25 <m.bhkm25@gmail.com>
2026-07-24 16:17:07 +0300

    Show customer business relationships in profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,1 +1,19 @@
-export { default } from './ProfileV2';
+import type { ComponentProps } from 'react';
+import MyBusinessRelationshipsOverview from './business/MyBusinessRelationshipsOverview';
+import ProfileV2 from './ProfileV2';
+
+type Props = ComponentProps<typeof ProfileV2>;
+
+export default function Profile(props: Props) {
+  const isOverview = (() => {
+    const path = window.location.pathname.replace(/\/+$/, '');
+    return path.endsWith('/profile') || path === 'profile';
+  })();
+
+  return (
+    <>
+      <ProfileV2 {...props} />
+      {isOverview && <MyBusinessRelationshipsOverview onNavigate={props.onNavigate} />}
+    </>
+  );
+}
