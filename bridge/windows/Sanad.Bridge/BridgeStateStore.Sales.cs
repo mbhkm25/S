b45dc9e223ede0d04ab21364e6bac942d8350939
mbhkm25 <244351525+mbhkm25@users.noreply.github.com>
2026-09-19 10:13:22 +0300

    release(bridge-main): add BridgeStateStore.Sales.cs

---
+++ bridge/windows/Sanad.Bridge/BridgeStateStore.Sales.cs
@@ -0,0 +1,41 @@
+namespace Sanad.Bridge
+{
+    internal static class BridgeStateStoreSalesExtensions
+    {
+        public static long? GetSaleWatermark(this BridgeStateStore state, string sourceKey)
+        {
+            using (var sales = new SaleStateStore())
+                return sales.GetWatermark(sourceKey);
+        }
+
+        public static void InitializeSaleWatermark(this BridgeStateStore state, string sourceKey, long invoiceId)
+        {
+            using (var sales = new SaleStateStore())
+                sales.InitializeWatermark(sourceKey, invoiceId);
+        }
+
+        public static bool QueueSaleBundle(this BridgeStateStore state, string sourceKey, long invoiceId, string revision, string eventId, string bodyJson)
+        {
+            using (var sales = new SaleStateStore())
+                return sales.QueueBundle(sourceKey, invoiceId, revision, eventId, bodyJson);
+        }
+
+        public static int CountPendingSaleEvents(this BridgeStateStore state, string sourceKey)
+        {
+            using (var sales = new SaleStateStore())
+                return sales.CountPending(sourceKey);
+        }
+
+        public static string GetObservedSaleRevision(this BridgeStateStore state, string sourceKey, long invoiceId)
+        {
+            using (var sales = new SaleStateStore())
+                return sales.GetObservedRevision(sourceKey, invoiceId);
+        }
+
+        public static void RecordObservedSaleRevision(this BridgeStateStore state, string sourceKey, long invoiceId, string revision)
+        {
+            using (var sales = new SaleStateStore())
+                sales.RecordObservedRevision(sourceKey, invoiceId, revision);
+        }
+    }
+}
